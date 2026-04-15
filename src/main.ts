03d3f0526c4bb4de65ba544b9b371c4264ba065f
Anderson Melo <142186594+AndersonMeloo@users.noreply.github.com>
2026-04-14 23:50:07 -0300

    feat(main.ts) - Configurando ValidationPipe Global - Personalização de Mensagens de Erro - Configurando Prexixo Global da API (/api)

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { BadRequestException, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      forbidNonWhitelisted: true,
+      transform: true,
+      exceptionFactory: (errors) => {
+        const messages = errors.flatMap((error) =>
+          error.constraints ? Object.values(error.constraints) : [],
+        );
+
+        return new BadRequestException(
+          messages.length > 0
+            ? messages.join(' | ')
+            : 'Dados de entrada inválidos',
+        );
+      },
+    }),
+  );
+
+  app.setGlobalPrefix('api');
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
